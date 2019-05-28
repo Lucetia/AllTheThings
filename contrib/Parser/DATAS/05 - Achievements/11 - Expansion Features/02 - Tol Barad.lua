a7cb0dd40db11f488df3db0b5a2685a50c7c2522
Lucetia <>
2019-05-28 02:33:52 -0700

    - Fixed some Tol Barad achievements.  Accidently used "achievement" rather than making it alliance only version.  Why no one yell at me before about this?!

--- contrib/Parser/DATAS/05 - Achievements/11 - Expansion Features/02 - Tol Barad.lua
+++ contrib/Parser/DATAS/05 - Achievements/11 - Expansion Features/02 - Tol Barad.lua
@@ -18,265 +18,265 @@ _.Achievements =
 						["hordeAchievementID"] = 5719,		-- Just Another Day in Tol Barad
 						["g"] = {
 							{	-- A Huge Problem
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 31,			-- A Huge Problem
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 31,					-- A Huge Problem
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- A Sticky Task
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 16,			-- A Sticky Task
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 16,					-- A Sticky Task
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- Bombs Away!
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 21,			-- Bombs Away!
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 21,					-- Bombs Away!
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Boosting Morale
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad [Alliance]
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad [Horde]
-								["criteriaID"] = 3,				-- Boosting Morale
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad [Alliance]
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad [Horde]
+								["criteriaID"] = 3,						-- Boosting Morale
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Cannonball!
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 11,			-- Cannonball!
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 11,					-- Cannonball!
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Captain P. Harris
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 1,				-- Captain P. Harris
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 1,						-- Captain P. Harris
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Claiming The Keep
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 5,				-- Claiming The Keep
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 5,						-- Claiming The Keep
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Clearing The Depths
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 22,			-- Clearing The Depths
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 22,					-- Clearing The Depths
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Cursed Shackles
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 27,			-- Cursed Shackles
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 27,					-- Cursed Shackles
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- D-Block
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 25,			-- D-Block
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 25,					-- D-Block
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- Finish The Job
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 18,			-- Finish The Job
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 18,					-- Finish The Job
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- First Lieutenant Connor
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 9,				-- First Lieutenant Connor
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 9,						-- First Lieutenant Connor
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Food From Below
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 30,			-- Food From Below
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 30,					-- Food From Below
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Ghostbuster
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 10,			-- Ghostbuster
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 10,					-- Ghostbuster
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- Learning From The Past
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 24,			-- Learning From The Past
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 24,					-- Learning From The Past
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Leave No Weapon Behind
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 6,				-- Leave No Weapon Behind
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 6,						-- Leave No Weapon Behind
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- Magnets, How Do They Work?
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 17,			-- Magnets, How Do They Work?
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 17,					-- Magnets, How Do They Work?
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Not The Friendliest Town
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 13,			-- Not The Friendliest Town
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 13,					-- Not The Friendliest Town
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Prison Revolt
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 28,			-- Prison Revolt
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 28,					-- Prison Revolt
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Rattling Their Cages
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 2,				-- Rattling Their Cages
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 2,						-- Rattling Their Cages
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- Salvaging The Remains
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 8,				-- Salvaging The Remains
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 8,						-- Salvaging The Remains
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Shark Tank
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 4,				-- Shark Tank
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 4,						-- Shark Tank
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Svarnos
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 26,			-- Svarnos
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 26,					-- Svarnos
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Swamp Bait
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 32,			-- Swamp Bait
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 32,					-- Swamp Bait
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- Taking The Overlook Back
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 12,			-- Taking The Overlook Back
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 12,					-- Taking The Overlook Back
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- Teach A Man To Fish.... Or Steal
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 14,			-- Teach A Man To Fish.... Or Steal
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 14,					-- Teach A Man To Fish.... Or Steal
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- The Forgotten
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 7,				-- The Forgotten
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 7,						-- The Forgotten
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- The Imprisoned Archmage
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 23,			-- The Imprisoned Archmage
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 23,					-- The Imprisoned Archmage
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- The Leftovers
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 33,			-- The Leftovers
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 33,					-- The Leftovers
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- The Warden
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 29,			-- The Warden
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 29,					-- The Warden
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- Thinning The Brood
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 15,			-- Thinning The Brood
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 15,					-- Thinning The Brood
 								["maps"] = {
 									244,	-- Tol Barad
 								},
 							},
 							{	-- WANTED: Foreman Wellson
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 20,			-- WANTED: Foreman Wellson
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 20,					-- WANTED: Foreman Wellson
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
 							},
 							{	-- Watch Out For Splinters!
-								["achievementID"] = 5718,		-- Just Another Day in Tol Barad
-								["hordeAchievementID"] = 5719,	-- Just Another Day in Tol Barad
-								["criteriaID"] = 19,			-- Watch Out For Splinters!
+								["allianceAchievementID"] = 5718,		-- Just Another Day in Tol Barad
+								["hordeAchievementID"] = 5719,			-- Just Another Day in Tol Barad
+								["criteriaID"] = 19,					-- Watch Out For Splinters!
 								["maps"] = {
 									245,	-- Tol Barad Peninsula
 								},
