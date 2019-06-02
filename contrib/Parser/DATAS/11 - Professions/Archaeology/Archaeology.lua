0fbbd0096a3d12ffc22df4f5ec543fe97d984b63
Lucetia <>
2019-06-02 11:39:05 -0700

    - Update to master

--- contrib/Parser/DATAS/11 - Professions/Archaeology/Archaeology.lua
+++ contrib/Parser/DATAS/11 - Professions/Archaeology/Archaeology.lua
@@ -1,344 +1,294 @@
 -- Archaeology - Skill ID 794 / Spell ID 158762
 profession(794, {	-- Archaeology
 	currency(829,  {	-- Arakkoa
-		["g"] = {
-			{	-- Arakkoa Cipher
-				["itemID"] = 109585,	-- Arakkoa Cipher
-				["g"] = {
-					sp(172460, { i(117354) }),	-- Ancient Nest Guardian
-					sp(168331, { i(117382) }),	-- Beakbreaker of Terokk
-				},
-			},
+		n(-162, {	-- Pets
+			sp(172460, { i(117354) }),	-- Ancient Nest Guardian
+		}),
+		n(-319, {	-- Weapons
+			sp(168331, { i(117382) }),	-- Beakbreaker of Terokk
+		}),
+	}),
+	i(64657, {
+		["groups"] = {
+			i(67538),	-- Recipe: Vial of the Sands
 		},
+		["requireSkill"] = 171,	-- Alchemy,
 	}),
 	currency(1174, {	-- Demonic
 		n(-17, {	-- Quests
-			["g"] = {
-				{	-- Mark of the Deceiver
-					["itemID"] = 130905,	-- Mark of the Deceiver
-					["g"] = {
-						q(41157, {	-- Corrupted Studies
-							["isWeekly"] = true,
-							["g"] = {
-								i(136362, {	-- Ancient War Remnants
-									i(131724),	-- Crystalline Eye of Undravius
-								}),
-							},
-						}),
-						q(41163, {	-- The Apocalypse Bringer
-							["isWeekly"] = true,
-							["g"] = {
-								i(136922),	-- Wyrmy Tunkins
-							},
-						}),
-					},
+			q(41157, {	-- Corrupted Studies
+				["isWeekly"] = true,
+				["groups"] = {
+					i(136362, {	-- Ancient War Remnants
+						i(131724),	-- Crystalline Eye of Undravius
+					}),
 				},
-			},
+			}),
+			q(41163, {	-- The Apocalypse Bringer
+				["isWeekly"] = true,
+				["groups"] = {
+					i(136922),	-- Wyrmy Tunkins
+				},
+			}),
 		}),
-		sp(196481, {
+		sp(196481, { 
 			i(130917),	-- Flayed-Skin Chronicle
 			i(130932),	-- Pristine Flayed-Skin Chronicle
 		}),
-		sp(196484, {
+		sp(196484, { 
 			i(130920),	-- Houndstooth Hauberk
 			i(130935),	-- Pristine Houndstooth Hauberk
 		}),
-		sp(196480, {
+		sp(196480, { 
 			i(130916),	-- Imp's Cup
 			i(130931),	-- Pristine Imp's Cup
 		}),
-		sp(196482, {
+		sp(196482, { 
 			i(130918),	-- Malformed Abyssal
 			i(130933),	-- Pristine Malformed Abyssal
 		}),
-		sp(196483, {
+		sp(196483, { 
 			i(130919),	-- Orb of Inner Chaos
 			i(130934),	-- Pristine Orb of Inner Chaos
-		}),
+		}),	
 	}),
 	currency(398,  {	-- Draenei
-		["g"] = {
-			{	-- Draenei Tome
-				["itemID"] = 64394,	-- Draenei Tome
-				["g"] = {
-					sp(90983, { i(64456) }),	-- Arrival of the Naaru
-				},
-			},
-		},
+		n(-161, {	-- Toys
+			sp(90983, { i(64456) }),	-- Arrival of the Naaru
+		}),
+		sp(90984, { i(64457) }),	-- The Last Relic of Argus
 	}),
 	currency(821,  {	-- Draenor Clans
-		["g"] = {
-			{	-- Draenor Clan Orator Cane
-				["itemID"] = 108439,	-- Draenor Clan Orator Cane
-				["g"] = {
-					sp(172459, { i(116985) }),	-- Headdress of the First Shaman
-					sp(172466, { i(117380) }),	-- Frostwolf Ghostpup
-				},
-			},
-		},
+		n(-318, {	-- Armor
+			sp(172459, { i(116985) }),	-- Headdress of the First Shaman
+		}),
+		n(-162, {	-- Pets
+			sp(172466, { i(117380) }),	-- Frostwolf Ghostpup
+		}),
 	}),
 	currency(1535, {	-- Drust
-		["g"] = {
-			{	-- Etched Drust Bone
-				["itemID"] = 154990,	-- Etched Drust Bone
-				["g"] = {
-					sp(273854, { i(161089) }),	-- Restored Revenant
-					sp(273852, { i(160751) }),	-- Dance of the Dead
-					i(160741),	-- Soul Coffer
-					i(160742),	-- Pristine Soul Coffer
-				},
-			},
-		},
+		n(-162, {	-- Pets
+			sp(273854, { i(161089) }),	-- Restored Revenant
+		}),
+		n(-161, {	-- Toys
+			sp(273852, { i(160751) }),	-- Dance of the Dead
+		}),
+		n(-386, {	-- Trinket
+			sp(273855, { i(160833) }),	-- Fetish of the Tormented Mind
+		}),
+		sp(273851, {	-- Soul Coffer (Spell #273851)
+			i(160741),	-- Soul Coffer
+			i(160742),	-- Pristine Soul Coffer
+		}),
 	}),
 	currency(384,  {	-- Dwarf
-		["g"] = {
-			{	-- Dwarf Rune Stone
-				["itemID"] = 52843,	-- Dwarf Rune Stone
-				["g"] = {
-					sp(90521, { i(64372) }),	-- Clockwork Gnome
-					sp(90553, { i(64373) }),	-- Chalice of the Mountain Kings
-					sp(91226, { i(64488) }),	-- The Innkeeper's Daughter
-					sp(91227, { i(64489) }),	-- Staff of Sorcerer-Thane Thaurissan
-				},
-			},
-		},
+		n(-162, {	-- Pets
+			sp(90521, { i(64372) }),	-- Clockwork Gnome
+		}),
+		n(-161, {	-- Toys
+			sp(90553, { i(64373) }),	-- Chalice of the Mountain Kings
+			sp(91226, { i(64488) }),	-- The Innkeeper's Daughter
+		}),
+		n(-319, {	-- Weapons
+			sp(91227, { i(64489) }),	-- Staff of Sorcerer-Thane Thaurissan
+		}),
 	}),
 	currency(393,  {	-- Fossil
-		["g"] = {
+		n(-318, {	-- Armor
 			sp(98533, { i(69764) }),	-- Extinct Turtle Shell
+		}),
+		n(-160, {	-- Mounts
 			sp(90619, { i(60954) }),	-- Fossilized Raptor
+		}),
+		n(-162, {	-- Pets
 			sp(89693, { i(60955) }),	-- Fossilized Hatchling
 			sp(98582, { i(69821) }),	-- Pterrordax Hatchling
+		}),
+		n(-161, {	-- Toys
 			sp(98560, { i(69776) }),	-- Ancient Amber
-		},
+		}),
 	}),
 	currency(1172, {	-- Highborne
-		["g"] = {
-			{	-- Ancient Suramar Scroll
-				["itemID"] = 130903,	-- Ancient Suramar Scroll
-				["g"] = {
-					i(137782),	-- Vantus Rune Technique: Grand Magistrix Elisande [Rank 3]
-					sp(196471, {	-- Inert Leystone Charm
-						i(130907),	-- Inert Leystone Charm
-						i(130922),	-- Pristine Inert Leystone Charm
-					}),
-					sp(196474, {	-- Nobleman's Letter Opener
-						i(130910),	-- Nobleman's Letter Opener
-						i(130925),	-- Pristine Nobleman's Letter Opener
-					}),
-					sp(196472, {	-- Quietwine Vial
-						i(130908),	-- Quietwine Vial
-						i(130923),	-- Pristine Quietwine Vial
-					}),
-					sp(196473, {	-- Pre-War Highborne Tapestry
-						i(130909),	-- Pre-War Highborne Tapestry
-						i(130924),	-- Pristine Pre-War Highborne Tapestry
-					}),
-					sp(196470, { 	-- Violetglass Vessel
-						i(130906),	--  Violetglass Vessel
-						i(130921),	--  Pristine Violetglass Vessel
-					}),
-				},
-			},
-		},
+		i(137782),	-- Vantus Rune Technique: Grand Magistrix Elisande [Rank 3]
+		sp(196471, {	-- Inert Leystone Charm
+			i(130907),	-- Inert Leystone Charm
+			i(130922),	-- Pristine Inert Leystone Charm
+		}),
+		sp(196474, {	-- Nobleman's Letter Opener
+			i(130910),	-- Nobleman's Letter Opener
+			i(130925),	-- Pristine Nobleman's Letter Opener
+		}),	
+		sp(196472, {	-- Quietwine Vial
+			i(130908),	-- Quietwine Vial
+			i(130923),	-- Pristine Quietwine Vial
+		}),	
+		sp(196473, {	-- Pre-War Highborne Tapestry
+			i(130909),	-- Pre-War Highborne Tapestry
+			i(130924),	-- Pristine Pre-War Highborne Tapestry
+		}),
+		sp(196470, { 	-- Violetglass Vessel
+			i(130906),	--  Violetglass Vessel
+			i(130921),	--  Pristine Violetglass Vessel
+		}),
 	}),
 	currency(1173, {	-- Highmountain Tauren
-		["g"] = {
-			{	-- Highmountain Ritual-Stone
-				["itemID"] = 130904,	-- Highmountain Ritual-Stone
-				["g"] = {
-					n(-17, {	-- Quests
-						q(41193, {	-- Laying to Rest
-							["isWeekly"] = true,
-							["g"] = {
-								i(131734),	-- Spirit of Eche'ro
-							},
-						}),
-						q(41188, {	-- Neltharion's Lair: Misdirected
-							["isWeekly"] = true,
-							["g"] = {
-								i(131733),	-- Spear of Rethu
-							},
-						}),
-					}),
-					i(137867),	-- Design: Shadowruby Band [Rank 3]
-					sp(196478, {
-						i(130914),	-- Drogbar Gem-Roller
-						i(130929),	-- Pristine Drogbar Gem-Roller
-					}),
-					sp(196477, {
-						i(130913),	-- Hand-Smoothed Pyrestone
-						i(130913),	-- Pristine Hand-Smoothed Pyrestone
-					}),
-					sp(196476, {
-						i(130912),	-- Moosebone Fish-Hook
-						i(130927),	-- Pristine Moosebone Fish-Hook
-					}),
-					sp(196479, {
-						i(130915),	-- Stonewood Bow
-						i(130930),	-- Pristine Stonewood Bow
-					}),
-					sp(196475, {
-						i(130911),	-- Trailhead Drum
-						i(130926),	-- Pristine Trailhead Drum
-					}),
+		n(-17, {	-- Quests
+			q(41193, {	-- Laying to Rest
+				["isWeekly"] = true,
+				["groups"] = {
+					i(131734),	-- Spirit of Eche'ro
 				},
-			},
-		},
+			}),
+			q(41188, {	-- Neltharion's Lair: Misdirected
+				["isWeekly"] = true,
+				["groups"] = {
+					i(131733),	-- Spear of Rethu
+				},
+			}),
+		}),
+		i(137867),	-- Design: Shadowruby Band [Rank 3]
+		sp(196478, { 
+			i(130914),	-- Drogbar Gem-Roller
+			i(130929),	-- Pristine Drogbar Gem-Roller
+		}),	
+		sp(196477, { 
+			i(130913),	-- Hand-Smoothed Pyrestone
+			i(130913),	-- Pristine Hand-Smoothed Pyrestone
+		}),
+		sp(196476, { 
+			i(130912),	-- Moosebone Fish-Hook
+			i(130927),	-- Pristine Moosebone Fish-Hook
+		}),
+		sp(196479, { 
+			i(130915),	-- Stonewood Bow
+			i(130930),	-- Pristine Stonewood Bow
+		}),	
+		sp(196475, { 
+			i(130911),	-- Trailhead Drum
+			i(130926),	-- Pristine Trailhead Drum
+		}),
 	}),
 	currency(754,  {	-- Mantid
-		["g"] = {
-			{	-- Mantid Amber Silver
-				["itemID"] = 95373,	-- Mantid Amber Silver
-				["g"] = {
-					sp(139786, { i(95391) }),	-- Mantid Sky Reaver
-					sp(139787, { i(95392) }),	-- Sonic Pulse Generator
-				},
-			},
-		},
+		n(-319, {	-- Weapons
+			sp(139786, { i(95391) }),	-- Mantid Sky Reaver
+			sp(139787, { i(95392) }),	-- Sonic Pulse Generator
+		}),
 	}),
 	currency(677,  {	-- Mogu
-		["g"] = {
-			{	-- Mogu Statue Piece
-				["itemID"] = 79869,	-- Mogue Statue Piece
-				["g"] = {
-					sp(113993, { i(89614) }),	-- Anatomical Dummy
-				},
-			},
-		},
+		n(-161, {	-- Toys
+			sp(113993, { i(89614) }),	-- Anatomical Dummy
+		}),
+		n(-386, {	-- Trinket
+			sp(113992, { i(89611) }),	-- Quilen Statuette
+		}),
 	}),
 	currency(400,  {	-- Nerubian
-		["g"] = {
-			{	-- Nerubian Obelisk
-				["itemID"] = 64396,	-- Nerubian Obelisk
-				["g"] = {
-					sp(91214, { i(64481) }),	-- Blessing of the Old God
-					sp(91215, { i(64482) }),	-- Puzzle Box of Yogg-Saron
-				},
-			},
-		},
+		n(-161, {	-- Toys
+			sp(91214, { i(64481) }),	-- Blessing of the Old God
+			sp(91215, { i(64482) }),	-- Puzzle Box of Yogg-Saron
+		}),
 	}),
 	currency(394,  {	-- Night Elf
-		["g"] = {
-			{	-- Highborne Scroll
-				["itemID"] = 63127,	-- Highborne Scroll
-				["g"] = {
-					sp(90616, { i(64643) }),	-- Queen Azshara's Dressing Gown
-					sp(91761, { i(64646) }),	-- Bones of Transformation
-					sp(90493, { i(64361) }),	-- Druid and Priest Statue Set
-					sp(90464, { i(64358) }),	-- Highborne Soul Mirror
-					sp(90614, { i(64383) }),	-- Kaldorei Wind Chimes
-					sp(91773, { i(64651) }),	-- Wisp Amulet
-					sp(91757, {		-- Tyrande's Favorite Doll
-						i(64645),	-- Tyrande's Favorite Doll
-					}),
-				},
-			},
-		},
+		n(-318, {	-- Armor
+			sp(90616, { i(64643) }),	-- Queen Azshara's Dressing Gown
+		}),
+		n(-161, {	-- Toys
+			sp(91761, { i(64646) }),	-- Bones of Transformation
+			sp(90493, { i(64361) }),	-- Druid and Priest Statue Set
+			sp(90464, { i(64358) }),	-- Highborne Soul Mirror
+			sp(90614, { i(64383) }),	-- Kaldorei Wind Chimes
+			sp(91773, { i(64651) }),	-- Wisp Amulet
+		}),
+		n(-386, {	-- Trinket
+			sp(91757, {		-- Tyrande's Favorite Doll
+				i(64645),	-- Tyrande's Favorite Doll
+			}),
+		}),
 	}),
 	currency(828,  {	-- Ogre
-		["g"] = {
-			{	-- Ogre Missive
-				["itemID"] = 109584,	-- Ogre Missive
-				["g"] = {
-					sp(168320, { i(117384) }),	-- Warmaul of the Warmaul Chieftain
-				},
-			},
-		},
+		n(-384, {	-- Neck
+			sp(168319, { i(117385) }),	-- Sorcerer-King Toe Ring
+		}),
+		n(-319, {	-- Weapon
+			sp(168320, { i(117384) }),	-- Warmaul of the Warmaul Chieftain
+		}),
 	}),
 	currency(397,  {	-- Orc
-		["g"] = {
-			{	-- Orc Blood Text
-				["itemID"] = 64392,	-- Orc Blood Text
-				["g"] = {
-					sp(90843, { i(64644) }),	-- Headdress of the First Shaman
-				},
-			},
-		},
+		n(-318, {	-- Armor
+			sp(90843, { i(64644) }),	-- Headdress of the First Shaman
+		}),
 	}),
 	currency(676,  {	-- Pandaren
-		["g"] = {
-			{	-- Pandaren Pottery Shard
-				["itemID"] = 79868,	-- Pandaren Pottery Shard
-				["g"] = {
-					sp(113981, { 	-- Spear of Xuen
-						i(89685), 	-- Spear of Xuen
-					}),
-					sp(113980, { 	-- Umbrella of Chi-Ji
-						i(89684),  	-- Umbrella of Chi-Ji
-					}),
-					sp(113971, {	-- Pandaren Game Board
-						i(79897),	-- Pandaren Game Board
-						i(85558),	-- Pristine Pandaren Game Board
-					}),
-					sp(113968, {	-- Pandaren Tea Set
-						i(79896),	-- Pandaren Tea Set
-						i(85557),	-- Pristine Pandaren Tea Set
-					}),
-				},
-			},
-		},
+		n(-319, {	-- Weapons
+			sp(113981, { 	-- Spear of Xuen
+				i(89685), 	-- Spear of Xuen
+			}),
+			sp(113980, { 	-- Umbrella of Chi-Ji
+				i(89684),  	-- Umbrella of Chi-Ji
+			}),	
+		}),
+		sp(113971, {	-- Pandaren Game Board 
+			i(79897),	-- Pandaren Game Board 
+			i(85558),	-- Pristine Pandaren Game Board 
+		}),
+		sp(113968, {	-- Pandaren Tea Set
+			i(79896),	-- Pandaren Tea Set
+			i(85557),	-- Pristine Pandaren Tea Set
+		}),
 	}),
 	currency(401,  {	-- Tol'vir
-		["g"] = {
-			{	-- Tol'vir Hieroglyphic
-				["itemID"] = 64397,	-- Tol'vir Hieroglyphic
-				["g"] = {
-					i(64657, {	-- Canopic Jar
-						["g"] = {
-							i(67538),	-- Recipe: Vial of the Sands
-						},
-						["requireSkill"] = 171,	-- Alchemy,
-					}),
-					sp(92148, { i(64883) }),	-- Scepter of Azj'Aqir
-					sp(92137, { i(60847) }),	-- Crawling Claw
-					sp(92145, { i(64881) }),	-- Pendant of the Scarab Storm
-					sp(92163, { i(64885) }),	-- Scimitar of the Sirocco
-					sp(92139, { i(64880) }),	-- Staff of Ammunae
-				},
-			},
-		},
+		n(-160, {	-- Mounts
+			sp(92148, { i(64883) }),	-- Scepter of Azj'Aqir
+		}),
+		n(-162, {	-- Pets
+			sp(92137, { i(60847) }),	-- Crawling Claw
+		}),
+		n(-161, {	-- Toys
+			sp(92145, { i(64881) }),	-- Pendant of the Scarab Storm
+		}),
+		n(-385, {	-- Finger
+			sp(92168, { i(64904) }),	-- Ring of the Boy Emperor
+		}),
+		n(-319, {	-- Weapons
+			sp(92163, { i(64885) }),	-- Scimitar of the Sirocco
+			sp(92139, { i(64880) }),	-- Staff of Ammunae
+		}),
 	}),
 	currency(385,  {	-- Troll
-		["g"] = {
-			{	-- Troll Tablet
-				["itemID"] = 63128,	-- Troll Tablet
-				["g"] = {
-					sp(98588, { i(69824) }),	-- Voodoo Figurine
-					sp(98556, { i(69777) }),	-- Haunted War Drum
-					sp(90608, { i(64377) }),	-- Zin'rokh, Destroyer of Worlds
-					sp(88262, {
-						i(63115),	-- Zandalari Voodoo Doll
-					}),
-				},
-			},
-		},
+		n(-162, {	-- Pets
+			sp(98588, { i(69824) }),	-- Voodoo Figurine
+		}),
+		n(-161, {	-- Toys
+			sp(98556, { i(69777) }),	-- Haunted War Drum
+		}),
+		n(-319, {	-- Weapons
+			sp(90608, { i(64377) }),	-- Zin'rokh, Destroyer of Worlds
+		}),
+		sp(88262, { 
+			i(63115),	-- Zandalari Voodoo Doll
+		}),
 	}),
 	currency(399,  {	-- Vrykul
-		["g"] = {
-			{	-- Vrykul Rune Stick
-				["itemID"] = 64395,	-- Vrykul Rune Stick
-				["g"] = {
-					sp(98569, { i(69775) }),	-- Vrykul Drinking Horn
-					sp(90997, { i(64460) }),	-- Nifflevar Bearded
-				},
-			},
-		},
+		n(-161, {	-- Toys
+			sp(98569, { i(69775) }),	-- Vrykul Drinking Horn
+		}),
+		n(-319, {	-- Weapons
+			sp(90997, { i(64460) }),	-- Nifflevar Bearded Axe
+		}),
 	}),
 	currency(1534, {	-- Zandalari
-		["g"] = {
-			{	-- Zandalari Idol
-				["itemID"] = 154989,	-- Zandalari Idol
-				["g"] = {
-					sp(273817, { i(161080) }),	-- Intact Direhorn Egg
-					sp(273815, { i(160740) }),	-- Croak Crock
-					sp(273819, { i(160753) }),	-- Sanguinating Totem (Trinket)}),
-					sp(273812, {	-- Blowgun of the Sethra
-						i(160743),	-- Blowgun of the Sethrak
-						i(160744),	-- Pristine Blowgun of the Sethrak
-					}),
-				},
-			},
-		},
+		n(-162, {	-- Pets
+			sp(273817, { i(161080) }),	-- Intact Direhorn Egg
+		}),
+		n(-161, {	-- Toys
+			sp(273815, { i(160740) }),	-- Croak Crock
+		}),
+		n(-386, {	-- Trinket
+			sp(273819, { i(160753) }),	-- Sanguinating Totem (Trinket)
+		}),
+		sp(273812, {	-- Blowgun of the Sethra
+			i(160743),	-- Blowgun of the Sethrak
+			i(160744),	-- Pristine Blowgun of the Sethrak
+		}),
 	}),
 });
