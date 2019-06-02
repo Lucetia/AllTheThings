036362de46c03e34fc1e619abe205934bbb34828
Lucetia <>
2019-06-02 12:50:47 -0700

    - Draenor Clans

---
+++ contrib/Parser/DATAS/11 - Professions/Archaeology/Draenor Clans.lua
@@ -0,0 +1,172 @@
+profession(794, {	-- Archaeology
+	{	-- Draenor Clans Fragments [Currency ID: 821]
+		["currencyID"] = 821,	-- Draenor Clans Fragments
+		["g"] = {
+			{	-- Draenor Clan Orator Cane
+				["itemID"] = 108439,	-- Draenor Clan Orator Cane
+				["g"] = {
+					{	-- Frostwolf Ghostpup [Item ID:117380; Spell ID:172466]
+						["itemID"] = 117380,	-- Frostwolf Ghostpup
+						["spellID"] = 172466,	-- Frostwolf Ghostpup
+					},
+					{	-- Headdress of the First Shaman [Item ID:116985; Spell ID:172459]
+						["itemID"] = 116985,	-- Headdress of the First Shaman
+						["spellID"] = 172459,	-- Headdress of the First Shaman
+					},
+					{	-- Pristine Ancestral Talisman [Item ID:114172; Quest ID:36756]
+						["itemID"] = 114172,	-- Pristine Ancestral Talisman
+						["questID"] = 36756,	-- Pristine Ancestral Talisman
+					},
+					{	-- Pristine Barbed Fishing Hook [Item ID:114164; Quest ID:36753]
+						["itemID"] = 114164,	-- Pristine Barbed Fishing Hook
+						["questID"] = 36753,	-- Pristine Barbed Fishing Hook
+					},
+					{	-- Pristine Blackrock Razor [Item ID:114158; Quest ID:36750]
+						["itemID"] = 114158,	-- Pristine Blackrock Razor
+						["questID"] = 36750,	-- Pristine Blackrock Razor
+					},
+					{	-- Pristine Calcified Eye In a Jar [Item ID:114166; Quest ID:36754]
+						["itemID"] = 114166,	-- Pristine Calcified Eye In a Jar
+						["questID"] = 36754,	-- Pristine Calcified Eye In a Jar
+					},
+					{	-- Pristine Ceremonial Tattoo Needles [Item ID:114168; Quest ID:36755]
+						["itemID"] = 114168,	-- Pristine Ceremonial Tattoo Needles
+						["questID"] = 36755,	-- Pristine Ceremonial Tattoo Needles
+					},
+					{	-- Pristine Cracked Ivory Idol [Item ID:114170; Quest ID:36757]
+						["itemID"] = 114170,	-- Pristine Cracked Ivory Idol
+						["questID"] = 36757,	-- Pristine Cracked Ivory Idol
+					},
+					{	-- Pristine Doomsday Prophecy [Item ID:114178; Quest ID:36760]
+						["itemID"] = 114178,	-- Pristine Doomsday Prophecy
+						["questID"] = 36760,	-- Pristine Doomsday Prophecy
+					},
+					{	-- Pristine Elemental Bellows [Item ID:114156; Quest ID:36749]
+						["itemID"] = 114156,	-- Pristine Elemental Bellows
+						["questID"] = 36749,	-- Pristine Elemental Bellows
+					},
+					{	-- Pristine Fang-Scarred Frostwolf Axe [Item ID:114142; Quest ID:36725]
+						["itemID"] = 114142,	-- Pristine Fang-Scarred Frostwolf Axe
+						["questID"] = 36725,	-- Pristine Fang-Scarred Frostwolf Axe
+					},
+					{	-- Pristine Flask of Blazegrease [Item ID:114174; Quest ID:36758]
+						["itemID"] = 114174,	-- Pristine Flask of Blazegrease
+						["questID"] = 36758,	-- Pristine Flask of Blazegrease
+					},
+					{	-- Pristine Frostwolf Ancestry Scrimshaw [Item ID:114144; Quest ID:36743]
+						["itemID"] = 114144,	-- Pristine Frostwolf Ancestry Scrimshaw
+						["questID"] = 36743,	-- Pristine Frostwolf Ancestry Scrimshaw
+					},
+					{	-- Pristine Gronn-Tooth Necklace [Item ID:114176; Quest ID:36759]
+						["itemID"] = 114176,	-- Pristine Gronn-Tooth Necklace
+						["questID"] = 36759,	-- Pristine Gronn-Tooth Necklace
+					},
+					{	-- Pristine Hooked Dagger [Item ID:114162; Quest ID:36752]
+						["itemID"] = 114162,	-- Pristine Hooked Dagger
+						["questID"] = 36752,	-- Pristine Hooked Dagger
+					},
+					{	-- Pristine Metalworker's Hammer [Item ID:114154; Quest ID:36748]
+						["itemID"] = 114154,	-- Pristine Metalworker's Hammer
+						["questID"] = 36748,	-- Pristine Metalworker's Hammer
+					},
+					{	-- Pristine Screaming Bullroarer [Item ID:114150; Quest ID:36746]
+						["itemID"] = 114150,	-- Pristine Screaming Bullroarer
+						["questID"] = 36746,	-- Pristine Screaming Bullroarer
+					},
+					{	-- Pristine Warsinger's Drums [Item ID:114148; Quest ID:36745]
+						["itemID"] = 114148,	-- Pristine Warsinger's Drums
+						["questID"] = 36745,	-- Pristine Warsinger's Drums
+					},
+					{	-- Pristine Warsong Ceremonial Pike [Item ID:114152; Quest ID:36747]
+						["itemID"] = 114152,	-- Pristine Warsong Ceremonial Pike
+						["questID"] = 36747,	-- Pristine Warsong Ceremonial Pike
+					},
+					{	-- Pristine Weighted Chopping Axe [Item ID:114160; Quest ID:36751]
+						["itemID"] = 114160,	-- Pristine Weighted Chopping Axe
+						["questID"] = 36751,	-- Pristine Weighted Chopping Axe
+					},
+					{	-- Pristine Wolfskin Snowshoes [Item ID:114146; Quest ID:36744]
+						["itemID"] = 114146,	-- Pristine Wolfskin Snowshoes
+						["questID"] = 36744,	-- Pristine Wolfskin Snowshoes
+					},
+					{	-- Ancestral Talisman [Item ID:114171; Spell ID:36756]
+						["itemID"] = 114171,	-- Ancestral Talisman
+						["spellID"] = 36756,	-- Ancestral Talisman
+					},
+					{	-- Barbed Fishing Hook [Item ID:114163; Spell ID:36753]
+						["itemID"] = 114163,	-- Barbed Fishing Hook
+						["spellID"] = 36753,	-- Barbed Fishing Hook
+					},
+					{	-- Blackrock Razor [Item ID:114157; Spell ID:36750]
+						["itemID"] = 114157,	-- Blackrock Razor
+						["spellID"] = 36750,	-- Blackrock Razor
+					},
+					{	-- Calcified Eye In a Jar [Item ID:114165; Spell ID:36754]
+						["itemID"] = 114165,	-- Calcified Eye In a Jar
+						["spellID"] = 36754,	-- Calcified Eye In a Jar
+					},
+					{	-- Ceremonial Tattoo Needles [Item ID:114167; Spell ID:36755]
+						["itemID"] = 114167,	-- Ceremonial Tattoo Needles
+						["spellID"] = 36755,	-- Ceremonial Tattoo Needles
+					},
+					{	-- Cracked Ivory Idol [Item ID:114169; Spell ID:36757]
+						["itemID"] = 114169,	-- Cracked Ivory Idol
+						["spellID"] = 36757,	-- Cracked Ivory Idol
+					},
+					{	-- Doomsday Prophecy [Item ID:114177; Spell ID:36760]
+						["itemID"] = 114177,	-- Doomsday Prophecy
+						["spellID"] = 36760,	-- Doomsday Prophecy
+					},
+					{	-- Elemental Bellows [Item ID:114155; Spell ID:36749]
+						["itemID"] = 114155,	-- Elemental Bellows
+						["spellID"] = 36749,	-- Elemental Bellows
+					},
+					{	-- Fang-Scarred Frostwolf Axe [Item ID:114141; Spell ID:36725]
+						["itemID"] = 114141,	-- Fang-Scarred Frostwolf Axe
+						["spellID"] = 36725,	-- Fang-Scarred Frostwolf Axe
+					},
+					{	-- Flask of Blazegrease [Item ID:114173; Spell ID:36758]
+						["itemID"] = 114173,	-- Flask of Blazegrease
+						["spellID"] = 36758,	-- Flask of Blazegrease
+					},
+					{	-- Frostwolf Ancestry Scrimshaw [Item ID:114143; Spell ID:36743]
+						["itemID"] = 114143,	-- Frostwolf Ancestry Scrimshaw
+						["spellID"] = 36743,	-- Frostwolf Ancestry Scrimshaw
+					},
+					{	-- Gronn-Tooth Necklace [Item ID:114175; Spell ID:36759]
+						["itemID"] = 114175,	-- Gronn-Tooth Necklace
+						["spellID"] = 36759,	-- Gronn-Tooth Necklace
+					},
+					{	-- Hooked Dagger [Item ID:114161; Spell ID:36752]
+						["itemID"] = 114161,	-- Hooked Dagger
+						["spellID"] = 36752,	-- Hooked Dagger
+					},
+					{	-- Metalworker's Hammer [Item ID:114153; Spell ID:36748]
+						["itemID"] = 114153,	-- Metalworker's Hammer
+						["spellID"] = 36748,	-- Metalworker's Hammer
+					},
+					{	-- Screaming Bullroarer [Item ID:114149; Spell ID:36746]
+						["itemID"] = 114149,	-- Screaming Bullroarer
+						["spellID"] = 36746,	-- Screaming Bullroarer
+					},
+					{	-- Warsinger's Drums [Item ID:114147; Spell ID:36745]
+						["itemID"] = 114147,	-- Warsinger's Drums
+						["spellID"] = 36745,	-- Warsinger's Drums
+					},
+					{	-- Warsong Ceremonial Pike [Item ID:114151; Spell ID:36747]
+						["itemID"] = 114151,	-- Warsong Ceremonial Pike
+						["spellID"] = 36747,	-- Warsong Ceremonial Pike
+					},
+					{	-- Weighted Chopping Axe [Item ID:114159; Spell ID:36751]
+						["itemID"] = 114159,	-- Weighted Chopping Axe
+						["spellID"] = 36751,	-- Weighted Chopping Axe
+					},
+					{	-- Wolfskin Snowshoes [Item ID:114145; Spell ID:36744]
+						["itemID"] = 114145,	-- Wolfskin Snowshoes
+						["spellID"] = 36744,	-- Wolfskin Snowshoes
+					},
+				},
+			},
+		},
+	},
+});
