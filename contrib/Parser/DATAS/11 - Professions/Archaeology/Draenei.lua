dd0373a4a0d6741f1280179522925668674461a7
Lucetia <>
2019-06-02 12:40:11 -0700

    - Draenei Arch

---
+++ contrib/Parser/DATAS/11 - Professions/Archaeology/Draenei.lua
@@ -0,0 +1,47 @@
+profession(794, {	-- Archaeology
+	{	-- Draenei Fragments [Currency ID: 398]
+		["currencyID"] = 398,	-- Draenei Fragments
+		["g"] = {
+			{	-- Arrival of the Naaru [Item ID:64456; Spell ID:90983]
+				["itemID"] = 64456,		-- Arrival of the Naaru
+				["spellID"] = 90983,	-- Arrival of the Naaru
+			},
+			{	-- The Last Relic of Argus [Item ID:64457; Spell ID:90984]
+				["itemID"] = 64457,		-- The Last Relic of Argus
+				["spellID"] = 90984,	-- The Last Relic of Argus
+			},
+			{	-- Anklet with Golden Bells [Item ID:64440; Spell ID:90853]
+				["itemID"] = 64440,		-- Anklet with Golden Bells
+				["spellID"] = 90853,	-- Anklet with Golden Bells
+			},
+			{	-- Baroque Sword Scabbard [Item ID:64453; Spell ID:90860]
+				["itemID"] = 64453,		-- Baroque Sword Scabbard
+				["spellID"] = 90860,	-- Baroque Sword Scabbard
+			},
+			{	-- Carved Harp of Exotic Wood [Item ID:64442; Spell ID:90975]
+				["itemID"] = 64442,		-- Carved Harp of Exotic Wood
+				["spellID"] = 90975,	-- Carved Harp of Exotic Wood
+			},
+			{	-- Dignified Portrait [Item ID:64455; Spell ID:90974]
+				["itemID"] = 64455,		-- Dignified Portrait
+				["spellID"] = 90974,	-- Dignified Portrait
+			},
+			{	-- Fine Crystal Candelabra [Item ID:64454; Spell ID:90987]
+				["itemID"] = 64454,		-- Fine Crystal Candelabra
+				["spellID"] = 90987,	-- Fine Crystal Candelabra
+			},
+			{	-- Plated Elekk Goad [Item ID:64458; Spell ID:90864]
+				["itemID"] = 64458,		-- Plated Elekk Goad
+				["spellID"] = 90864,	-- Plated Elekk Goad
+			},
+			{	-- Scepter of the Nathrezim [Item ID:64444; Spell ID:90864]
+				["itemID"] = 64444,		-- Scepter of the Nathrezim
+				["spellID"] = 90864,	-- Scepter of the Nathrezim
+			},
+			{	-- Strange Silver Paperweight [Item ID:64443; Spell ID:90861]
+				["itemID"] = 64443,		-- Strange Silver Paperweight
+				["spellID"] = 90861,	-- Strange Silver Paperweight
+			},
+		},
+	},
+});
