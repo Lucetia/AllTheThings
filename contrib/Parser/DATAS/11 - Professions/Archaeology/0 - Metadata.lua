bd7d542fbf4ce5e679310905d6f3a61e83cf9dad
Lucetia <>
2019-06-02 12:04:02 -0700

    - Arrakkoa and Metadata

---
+++ contrib/Parser/DATAS/11 - Professions/Archaeology/0 - Metadata.lua
@@ -0,0 +1,4 @@
+-- Archaeology - Skill ID 794 / Spell ID 158762
+profession(794, {	-- Archaeology
+
+});
---
+++ contrib/Parser/DATAS/11 - Professions/Archaeology/Arakkoa.lua
@@ -0,0 +1,60 @@
+profession(794, {	-- Archaeology
+	{	-- Arrakoa
+		["currencyID"] = 829,	-- Arakkoa Fragments
+		["g"] = {
+			{	-- Arakkoa Cipher
+				["itemID"] = 109585,	-- Arakkoa Cipher
+				["g"] = {
+					{	-- Ancient Nest Guardian [Item ID:117354; Spell ID:172460]
+						["itemID"] = 117354,	-- Ancient Nest Guardian
+						["spellID"] = 172460,	-- Ancient Nest Guardian
+					},
+					{	-- Beakbreaker of Terokk [Item ID:117382; Spell ID:168331]
+						["itemID"] = 117382,	-- Beakbreaker of Terokk
+						["spellID"] = 168331,	-- Beakbreaker of Terokk
+					},
+					{	-- Pristine Apexis Crystal [Item ID:114222; Quest ID:36778]
+						["itemID"] = 114222,	-- Pristine Apexis Crystal
+						["questID"] = 36778,	-- Pristine Apexis Crystal
+					},
+					{	-- Pristine Apexis Hieroglyph [Item ID:114223; Quest ID:36779]
+						["itemID"] = 114223,	-- Pristine Apexis Hieroglyph
+						["questID"] = 36779,	-- Pristine Apexis Hieroglyph
+					},
+					{	-- Pristine Apexis Scroll [Item ID:114224; Quest ID:36780]
+						["itemID"] = 114224,	-- Pristine Apexis Scroll
+						["questID"] = 36780,	-- Pristine Apexis Scroll
+					},
+					{	-- Pristine Burial Urn [Item ID:114216; Quest ID:36772]
+						["itemID"] = 114216,	-- Pristine Burial Urn
+						["questID"] = 36772,	-- Pristine Burial Urn
+					},
+					{	-- Pristine Decree Scrolls [Item ID:114217; Quest ID:36773]
+						["itemID"] = 114217,	-- Pristine Decree Scrolls
+						["questID"] = 36773,	-- Pristine Decree Scrolls
+					},
+					{	-- Pristine Dreamcatcher [Item ID:114215; Quest ID:36771]
+						["itemID"] = 114215,	-- Pristine Dreamcatcher
+						["questID"] = 36771,	-- Pristine Dreamcatcher
+					},
+					{	-- Pristine Outcast Dreamcatcher [Item ID:114221; Quest ID:36777]
+						["itemID"] = 114221,	-- Pristine Outcast Dreamcatcher
+						["questID"] = 36777,	-- Pristine Outcast Dreamcatcher
+					},
+					{	-- Pristine Solar Orb [Item ID:114218; Quest ID:36774]
+						["itemID"] = 114218,	-- Pristine Solar Orb
+						["questID"] = 36774,	-- Pristine Solar Orb
+					},
+					{	-- Pristine Sundial [Item ID:114219; Quest ID:36775]
+						["itemID"] = 114219,	-- Pristine Sundial
+						["questID"] = 36775,	-- Pristine Sundial
+					},
+					{	-- Pristine Talonpriest Mask [Item ID:114220; Quest ID:36776]
+						["itemID"] = 114220,	-- Pristine Talonpriest Mask
+						["questID"] = 36776,	-- Pristine Talonpriest Mask
+					},
+				},
+			},
+		},
+	},
+});
